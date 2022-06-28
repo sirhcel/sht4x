7bfbf64bae3255c192aa06e27b3316ada0cf6ea8
Christian Meusel <christian.meusel@posteo.de>
2022-06-29 00:52:10 +0200

    Eliminate manics from impossible measurements
    The computations for millis are safe for all possible values returned
    from the sensor. So there is no need for complicating things by allowing
    "impossible" measurements from outside this domain.
    
    This change also harmonizes the interface to measurement values. All
    access goes through functions now.
    
    The test for zero (millis) got dropped because exact zero measurements
    are not possible for actual sensor data.

--- tests/measurement.rs
+++ tests/measurement.rs
@@ -1,4 +1,3 @@
-use fixed::types::I16F16;
 use sht4x::{Measurement, SensorData};
 
 const DATA_ZERO_ZERO: SensorData = SensorData {
@@ -11,59 +10,26 @@ const DATA_MAX_MAX: SensorData = SensorData {
     humidity: u16::MAX,
 };
 
-const MEASUREMENT_MIN_MIN: Measurement = Measurement {
-    temperature_celsius: I16F16::MIN,
-    humidity_percent: I16F16::MIN,
-};
-
-const MEASUREMENT_MAX_MAX: Measurement = Measurement {
-    temperature_celsius: I16F16::MAX,
-    humidity_percent: I16F16::MAX,
-};
-
 #[test]
 fn from_min_data() {
-    let min = Measurement::from(DATA_ZERO_ZERO);
-    assert_eq!(min.temperature_celsius, -45);
-    assert_eq!(min.humidity_percent, -6);
+    let min: Measurement = DATA_ZERO_ZERO.into();
+    assert_eq!(min.temperature_celsius(), -45);
+    assert_eq!(min.humidity_percent(), -6);
 }
 
 #[test]
 fn from_max_data() {
     let max = Measurement::from(DATA_MAX_MAX);
-    assert_eq!(max.temperature_celsius, -45 + 175);
-    assert_eq!(max.humidity_percent, -6 + 125);
-}
-
-#[test]
-#[should_panic]
-fn min_temperature_milli_celsius() {
-    assert!(MEASUREMENT_MIN_MIN.temperature_milli_celsius() < 0);
-}
-
-#[test]
-#[should_panic]
-fn min_humidity_milli_percent() {
-    assert!(MEASUREMENT_MIN_MIN.humidity_milli_percent() < 0);
+    assert_eq!(max.temperature_celsius(), -45 + 175);
+    assert_eq!(max.humidity_percent(), -6 + 125);
 }
 
 #[test]
 fn min_data_millis() {
-    let min_data = Measurement::from(DATA_ZERO_ZERO);
+    let min_data: Measurement = DATA_ZERO_ZERO.into();
 
     assert_eq!(min_data.temperature_milli_celsius(), -45000);
     assert_eq!(min_data.humidity_milli_percent(), -6000);
-}
-
-#[test]
-fn zero_millis() {
-    let zeroes = Measurement {
-        temperature_celsius: I16F16::ZERO,
-        humidity_percent: I16F16::ZERO,
-    };
-
-    assert_eq!(zeroes.temperature_milli_celsius(), 0);
-    assert_eq!(zeroes.humidity_milli_percent(), 0);
 }
 
 #[test]
@@ -72,16 +38,4 @@ fn max_data_millis() {
 
     assert_eq!(max_data.temperature_milli_celsius(), -45000 + 175000);
     assert_eq!(max_data.humidity_milli_percent(), -6000 + 125000);
-}
-
-#[test]
-#[should_panic]
-fn max_temperature_milli_celsius() {
-    assert!(MEASUREMENT_MAX_MAX.temperature_milli_celsius() > 0);
-}
-
-#[test]
-#[should_panic]
-fn max_humidity_milli_percent() {
-    assert!(MEASUREMENT_MAX_MAX.humidity_milli_percent() > 0);
 }
